3e4e5d44a52572af8bd2897eadfe82931a91ad2a
Lakshman Kumar <kumarlaksxman17@gmail.com>
2026-03-13 19:26:56 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ai-assisted-journal-finder-web1.onrender.com",
         changeOrigin: true,
       },
     },
